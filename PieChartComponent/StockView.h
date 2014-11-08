5fec76fb4207c56a56a286c737738676e8d82bb7
Matias Ginart <matiasginart@gmail.com>
2014-11-08 17:16:04 -0300

    Agregadas clases

---
+++ PieChartComponent/StockView.h
@@ -0,0 +1,22 @@
+//
+//  PieChartCollectionViewCell.h
+//  PieChartComponent
+//
+//  Created by Matías Ginart on 11/7/14.
+//  Copyright (c) 2014 Casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PieChartConfiguration.h"
+
+@interface StockView : UIView
+
++ (instancetype)view;
+
+- (void)setText:(NSString *)text;
+
+- (void)updateWithConfiguration:(PieChartConfiguration*)configuration;
+
+- (void)updateMoreStocksWithConfiguration:(PieChartConfiguration*)configuration;
+
+@end
---
+++ PieChartComponent/StockView.m
@@ -0,0 +1,44 @@
+//
+//  PieChartCollectionViewCell.m
+//  PieChartComponent
+//
+//  Created by Matías Ginart on 11/7/14.
+//  Copyright (c) 2014 Casa. All rights reserved.
+//
+
+#import "StockView.h"
+
+@interface StockView ()
+@property (nonatomic, weak) IBOutlet UIImageView* imageView;
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+@end
+
+@implementation StockView
+
++ (instancetype)view {
+	StockView *objectToReturn = [[NSBundle mainBundle] loadNibNamed:@"StockView" owner:nil options:nil][0];
+	return objectToReturn;
+}
+
+- (void)setText:(NSString *)text {
+	CGSize size = [text sizeWithAttributes:@{ NSFontAttributeName:self.titleLabel.font }];
+	self.titleLabel.text = text;
+    self.titleLabel.frame = CGRectMake(self.titleLabel.frame.origin.x, self.titleLabel.frame.origin.y, size.width, size.height);
+	self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, size.width + 9, size.height + 10);
+}
+
+- (void)updateWithConfiguration:(PieChartConfiguration*)configuration {
+    self.imageView.tintColor = configuration.itemSectorsColor;
+    //ASADO
+    self.imageView.backgroundColor = configuration.itemSectorsColor;
+    self.titleLabel.textColor = configuration.itemSectorsTextColor;
+    self.titleLabel.font = configuration.itemSectorsTextFont;
+}
+
+- (void)updateMoreStocksWithConfiguration:(PieChartConfiguration*)configuration {
+    self.imageView.tintColor = configuration.itemMoreSectorsColor;
+    //ASADO
+    self.imageView.backgroundColor = configuration.itemMoreSectorsColor;
+}
+
+@end
